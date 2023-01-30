c31b954281c6f1ad7a7e899c0c3d6b2a7b527ead
Yuriy Halytskyy <yuriy.halytskyy@nesi.org.nz>
2023-01-30 13:16:38 +1300

    fix bugs

--- plugins/modules/fs_info.py
+++ plugins/modules/fs_info.py
@@ -24,11 +24,13 @@ def main():
     name = module.params["name"]
     try:
         module.exit_json(changed=False, fs_info = FS(name)._dict_)
-    except IndexError:
-        module.fail_json(msg=f"filesystem {name} not found")
-    except ValueError:
-        module.fail_json(msg=f"{name} is not a valid filesystem name")
-    except Exception as e:
+    except IndexError as e:
+        module.fail_json(msg=f"filesystem {name} not found",
+                         exception = e)
+    except ValueError as e:
+        module.fail_json(msg=f"{name} is not a valid filesystem name",
+                         exception= e)
+    except Exception as e: # pylint: disable=broad-except
         module.fail_json(msg="fs_info failed", exception = e)
 
 if __name__ == '__main__':
