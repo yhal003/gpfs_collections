f88a4b3dd2d68dd513e6d4c2b7c5a93855302943
Yuriy Halytskyy <yuriy.halytskyy@nesi.org.nz>
2023-02-05 12:53:43 +1300

    fix bugs

--- plugins/module_utils/gpfs.py
+++ plugins/module_utils/gpfs.py
@@ -90,6 +90,7 @@ class FilesetQuota:
         quota.block_hard = int(props["fileset"][0]["blockLimit"])
         quota.file_soft = int(props["fileset"][0]["filesQuota"])
         quota.file_soft = int(props["fileset"][0]["filesLimit"])
+        return quota
 
 
     def __init__(self, filesystem, fileset_name,
